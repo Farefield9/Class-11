b160aacddeebe813f5163294876d959ada32f6d4
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-14 17:58:57 +0530

    Create a9.py

---
+++ a9.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("you are eligible to vote") 
+else:
+        print("You are not eligible to vote")
+num = int(input("Enter a number:"))
+if num>0:
+    print(num,"is a positive number")
+elif num<0:
+    print(num,"is a negative number")
+else:
+    print("num is 0")
