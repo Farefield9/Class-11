84d7c9a4a1b01500a5dcb42e1e7dd88cc761ff42
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-15 16:49:37 +0530

    Create a13.py

---
+++ a13.py
@@ -0,0 +1,10 @@
+a = [4,7,8,9,6,4,2,3,4,5]
+a = sum(list)
+print(a)
+dict = {'mohan':45,2.5:55}
+dict1 = dict['meena']=42
+print(dict)
+print(dict1)
+dict = {'ram':5,2.5:4}
+for i in dict:
+    print(i,':',dict[i])
