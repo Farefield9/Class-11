509f8d1daacd4ccbe804d84207e051bd07c88105
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-15 17:14:13 +0530

    Create a17.py

---
+++ a17.py
@@ -0,0 +1,6 @@
+text = input("Enter text")
+v = 0
+for i in text:
+    if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+        v = v + 1
+print("vowels",v)
