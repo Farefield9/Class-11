112dc42ecbd62df21d414f61bf8a5e1f0620a9ba
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-15 16:48:45 +0530

    Create a12.py

---
+++ a12.py
@@ -0,0 +1,9 @@
+list = [4,5,4,4,8,7,8,7]
+print(list[1:6:2])
+list1 = [4,5,8,6,4]
+list2 = [4,'ram',5.6]
+list3 = list1 +list2
+print(list3)
+list = [4,5,8,7,9,6,3,21,4]
+for i in list:
+    print(i)
