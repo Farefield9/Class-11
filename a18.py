a43ed59b8db0e2b32084af36158737b6733a95c8
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-15 17:15:14 +0530

    Create a18.py

---
+++ a18.py
@@ -0,0 +1,11 @@
+print(4% and //3)
+print(20%30//40+(14-2))
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+c = int(input("Enter a number"))
+d = a,b,c
+list1 = []
+list1.append(d)
+print(list1)
+list2 = list1.sort()
+print(list1)
