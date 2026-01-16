f2ee3e752f30dab0e4c50a8dab1a75a6983388f5
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-16 16:54:02 +0530

    Create a30.py

---
+++ a30.py
@@ -0,0 +1,10 @@
+nd = int(input("Enter number of days "))
+if nd <= 5:
+ amt = nd * 2
+elif nd >=6 and nd <=10:
+ amt = nd * 3
+elif nd >= 11 and nd <= 15:
+ amt = nd * 4
+else:
+ amt = nd * 5
+print("Total amount to pay is ", amt)
