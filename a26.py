42c42425b8d7b8bd63560cd2cc4ebbc9a0f0beee
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-16 16:50:16 +0530

    Create a26.py

---
+++ a26.py
@@ -0,0 +1,13 @@
+for i in range(1,11):
+    print(i*2)
+i = 1
+while i<11:
+        print(i*2)
+        i = i+1
+else:
+          print("ee")
+for i in range(1,11):
+    if i == 5:
+        break
+    else:
+        print(i)
