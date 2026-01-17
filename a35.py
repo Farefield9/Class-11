c884c2e4fbfadff253fb96397138d6926c6454fa
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-17 16:50:12 +0530

    Create a35.py

---
+++ a35.py
@@ -0,0 +1,8 @@
+l = int(input("Enter length"))
+b = int(input("Enter breadth"))
+rect = l*b
+print("area of rectangle",rect)
+l = int(input("Enter length"))
+b = int(input("Enter breadth"))
+rect = 2*(l+b)
+print("perimeter of rectangle",rect)
