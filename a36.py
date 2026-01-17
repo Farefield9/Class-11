5350243757b4a412ca56178199925a6a25fdaa70
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-01-17 16:50:41 +0530

    Create a36.py

---
+++ a36.py
@@ -0,0 +1,9 @@
+h = int(input("Enter height"))
+b = int(input("Enter base"))
+tri = 1/2*b*h
+print("area of triangle",tri)
+a = int(input("Enter 1 side"))
+b = int(input("Enter 2 side"))
+c = int(input("Enter 3 side"))
+tri = a+b+c
+print("The perimeter of triangle",tri)
