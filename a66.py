2966d9e53a9d274518c6cfc43899c718326a895b
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-02-11 17:09:42 +0530

    Create a66.py

---
+++ a66.py
@@ -0,0 +1,8 @@
+for i in range (1, 5) :
+    print ("A" * i , end = "")
+for i in range (1, 4) :
+    print ("B" * i , end = "")
+for i in range (1, 3) :
+    print ("C" * i , end = "")
+for i in range (1, 2) :
+    print ("E" * i , end = "")
