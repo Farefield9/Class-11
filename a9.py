b631a223403264d896f8da2a6690119469157e91
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-03-06 19:03:22 +0530

    Remove number classification logic from a9.py

--- a9.py
+++ a9.py
@@ -3,10 +3,4 @@ if age >= 18:
     print("you are eligible to vote") 
 else:
         print("You are not eligible to vote")
-num = int(input("Enter a number:"))
-if num>0:
-    print(num,"is a positive number")
-elif num<0:
-    print(num,"is a negative number")
-else:
-    print("num is 0")
+
