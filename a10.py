863999dbe2980f0ae56111bbfacb9a6b8e67d7ab
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-03-08 10:39:02 +0530

    Update a10.py

--- a10.py
+++ a10.py
@@ -2,10 +2,3 @@ a = [1,5,2,4,8,9,6,7,3,5,24]
 for i in a:
     if (i%2) == 0:
         print(i,"is even")
-
-for var1 in range(3):
-    print("iteration" + str(var1 +1) + "of outer loop")
-    for var2 in range(2):
-        print(var2 + 1)
-        print("out of inner loop")
-        print("out of outer loop")
